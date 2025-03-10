214834a4ed8404bb6e8cdd45be4fccec4d4d1dfa
liquidcode-dev <liquidcode.dev@gmail.com>
2025-03-10 11:54:13 +0900

    port

--- backend/server.js
+++ backend/server.js
@@ -183,6 +183,6 @@ app.get('/search-bandcamp', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port:${PORT}`);
 });
